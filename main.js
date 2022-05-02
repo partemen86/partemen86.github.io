4361fa1dc9501186917f92875554272828a683e4
partemen86 <65257376+partemen86@users.noreply.github.com>
2022-05-02 12:54:04 -0400

    added assets

---
+++ main.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("menu");
+const menuItems = [...document.getElementsByClassName("menuItem")];
+
+menu.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  menuItems.forEach(menuItem => {
+    menuItem.classList.toggle("hidden")
+    menuItem.classList.toggle("flex")
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: ["./**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      '4xl': '1800px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      height: {
+        "xl": "20rem",
+        "2xl": "24rem",
+        "3xl": "32rem"
+      },
+      colors: {
+        "bastionblue": {
+          "500":"#3350DE"
+        },
+        "bastionorange": "#E63300",
+      }
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("tw-elements/dist/plugin"),
+    require("tailwind-scrollbar-hide")
+  ],
+}
